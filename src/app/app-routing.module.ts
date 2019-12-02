46c1f79ddbfcbaa0ed2732b85707a0cf1a54dbc7
riomulyadi <57655196+riomulyadi@users.noreply.github.com>
2019-12-02 16:49:36 +0700

    Edit Halaman

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,14 @@ const routes: Routes = [
   {
     path: 'daftar',
     loadChildren: () => import('./page/daftar/daftar.module').then( m => m.DaftarPageModule)
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  },
+  {
+    path: 'tab5',
+    loadChildren: () => import('./tab5/tab5.module').then( m => m.Tab5PageModule)
   }
 ];
 @NgModule({
