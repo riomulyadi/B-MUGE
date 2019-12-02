b6ae01de507861bccc5c54ab3fb37ca7dd259167
riomulyadi <57655196+riomulyadi@users.noreply.github.com>
2019-12-02 10:53:16 +0700

    Tambah Halaman

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./page/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'daftar',
+    loadChildren: () => import('./page/daftar/daftar.module').then( m => m.DaftarPageModule)
   }
 ];
 @NgModule({
