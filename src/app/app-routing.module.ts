dfd0b7468c66c1357f8076c8ee29af571cbcb5ce
Rosdiana <rosdiana60681@gmail.com>
2019-12-31 07:05:43 +0700

    ros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,34 +2,6 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./page/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'daftar',
-    loadChildren: () => import('./page/daftar/daftar.module').then( m => m.DaftarPageModule)
-  },
-  {
-    path: 'tab4',
-    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
-  },
-  {
-    path: 'tab5',
-    loadChildren: () => import('./tab5/tab5.module').then( m => m.Tab5PageModule)
-  },  {
-    path: 'reset',
-    loadChildren: () => import('./page/reset/reset.module').then( m => m.ResetPageModule)
-  },
-
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
   { 
     path: '', redirectTo: 'login', pathMatch: 'full' 
   },
@@ -59,10 +31,10 @@ const routes: Routes = [
     loadChildren: () => import('./page/tokosaya/tokosaya.module').then( m => m.TokosayaPageModule)
   },
   {
-
     path: 'resetpassword',
     loadChildren: () => import('./page/resetpassword/resetpassword.module').then( m => m.ResetpasswordPageModule)
-
+  },
+  {
     path: 'orderan',
     loadChildren: () => import('./page/orderan/orderan.module').then( m => m.OrderanPageModule)
   }
